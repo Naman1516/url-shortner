fdf50239feea7c28d82d196cc6b13b2f9266c618
Naman Jawaliya <jawaliyan@gmail.com>
2023-11-27 22:45:58 +0530

    made it responsive

--- client/src/components/MainSubText.tsx
+++ client/src/components/MainSubText.tsx
@@ -4,7 +4,11 @@ type SubTextProps = {
 
 const MainSubText = (props: SubTextProps) => {
   const { subtext } = props;
-  return <p className="text-[#C9CED6] max-w-lg text-center text-sm">{subtext}</p>;
+  return (
+    <p className="text-[#C9CED6] max-w-xs md:max-w-md lg:max-w-lg text-center text-sm">
+      {subtext}
+    </p>
+  );
 };
 
 export default MainSubText;
