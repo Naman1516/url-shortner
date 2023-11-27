6421c08739df576f0c64eda6df85ed3556dc21dd
Naman Jawaliya <jawaliyan@gmail.com>
2023-11-27 19:19:04 +0530

    Getting brand name from constants

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,8 +1,10 @@
+import { BRAND_NAME } from "@/utils/constants";
+
 const Header = () => {
   return (
     <header className="bg-transparent h-16 flex justify-between items-center px-10 fixed inset-0 text-white">
       <h1 className="font-extrabold bg-gradient-to-r from-[#eb568e] to-[#144ee3] text-transparent bg-clip-text">
-        Linkly
+        {BRAND_NAME}
       </h1>
       {/* <div className="flex gap-10">
         <button>Login</button>
