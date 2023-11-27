71182bb583562e2056829c548883a9547c95cd0c
Naman Jawaliya <jawaliyan@gmail.com>
2023-11-27 19:29:35 +0530

    using BRAND_NAME for subtext

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -3,7 +3,7 @@ import { BRAND_NAME } from "@/utils/constants";
 const Header = () => {
   return (
     <header className="bg-transparent h-16 flex justify-between items-center px-10 fixed inset-0 text-white">
-      <h1 className="font-extrabold bg-gradient-to-r from-[#eb568e] to-[#144ee3] text-transparent bg-clip-text">
+      <h1 className="font-extrabold bg-gradient-to-r from-[#eb568e] to-[#144ee3] text-transparent bg-clip-text uppercase">
         {BRAND_NAME}
       </h1>
       {/* <div className="flex gap-10">
--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,5 +1,4 @@
+export const BRAND_NAME = "Nshortly";
 export const API_BASE_URL = "https://sh-rt.cyclic.app/";
 export const TAGLINE = "Shorten Your Loooong Links :)";
-export const SUBTEXT =
-  "Linkly is an efficient and easy-to-use URL shortening service that streamlines your online experience.";
-export const BRAND_NAME = "NSHORTLY";
+export const SUBTEXT = `${BRAND_NAME} is an efficient and easy-to-use URL shortening service that streamlines your online experience.`;
