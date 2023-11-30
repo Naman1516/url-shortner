d7be022e9d7bf3931ffcc5c7b5a188f9559631dc
Naman Jawaliya <jawaliyan@gmail.com>
2023-12-01 00:34:24 +0530

    using shadcn components

--- client/src/components/MainShortenSection.tsx
+++ client/src/components/MainShortenSection.tsx
@@ -1,21 +1,25 @@
 import SpinnerIcon from "@/components/icons/SpinnerIcon";
 import useGenerateShortenUrl from "@/utils/custom-hooks/useGenerateShortenUrl";
-import { ArrowRight, Link } from "lucide-react";
+import { ArrowRight, Copy, FileCheck2, Link } from "lucide-react";
 import { FormEvent, useState } from "react";
-import MainCopyShortLink from "@/components/MainCopyShortLink";
 import PrimaryButton from "@/components/buttons/PrimaryButton";
 import { useGetAllUrls } from "@/utils/custom-hooks/useGetAllUrls";
+import { Input } from "@/components/ui/input";
+import useCopyToClipboard from "@/utils/custom-hooks/useCopyToClipboard";
 
 type InputProps = {
   btnText: string;
 };
 
-const MainShortenSection = (props: InputProps) => {
+const MainShortenSection = ({ btnText }: InputProps) => {
   const [url, setUrl] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [shortLink, setShortLink] = useState("");
-  const getAllUrls = useGetAllUrls();
+  const [isCopyMode, setIsCopyMode] = useState(false);
+  const [isCopied, setIsCopied] = useState(false);
 
+  const getAllUrls = useGetAllUrls();
+  const { copyToClipboard } = useCopyToClipboard();
   const { generate } = useGenerateShortenUrl();
 
   const handleShortenUrl = async (event: FormEvent) => {
@@ -23,64 +27,78 @@ const MainShortenSection = (props: InputProps) => {
     if (!url) return;
     setIsLoading(true);
     const respone = await generate(url);
+    setIsCopyMode(true);
     if (respone) {
       setShortLink(respone.shortUrl);
-      setTimeout(clearshortLink, 7000);
+      setTimeout(() => {
+        setShortLink("");
+        setUrl("");
+        setIsCopyMode(false);
+      }, 7000);
     }
-    setUrl("");
     setIsLoading(false);
     getAllUrls();
   };
 
-  const clearshortLink = () => {
-    setShortLink("");
+  const handleCopy = async (event: FormEvent) => {
+    event.preventDefault();
+    try {
+      const response = await copyToClipboard(shortLink);
+      if (response) setIsCopied(true);
+      setTimeout(() => {
+        setIsCopied(false);
+      }, 2000);
+    } catch (error) {
+      console.error({ error });
+    }
   };
 
   return (
     <section className="w-full flex flex-col justify-center items-center">
-      {shortLink ? (
-        <MainCopyShortLink shortLink={shortLink} />
-      ) : (
-        <>
-          <form className="relative w-10/12 md:w-1/2 flex items-center justify-center text-white">
-            <Link size={16} color="#C9CED6" className="absolute left-6 opacity-40" />
-            <input
-              type="text"
-              placeholder="Enter your link"
-              className="rounded-full pr-16 lg:pr-44 w-full h-16 px-4 py-6 pl-12 bg-[#181e29] border-4 border-[#353c4a] placeholder:text-[#c9ced6] caret-[#EB568E] placeholder:opacity-40"
-              value={url}
-              onChange={(event) => setUrl(event.target.value)}
-            />
-            <div className="hidden lg:block">
-              <PrimaryButton
-                type="submit"
-                callback={(event: FormEvent) => handleShortenUrl(event)}
-                disabled={isLoading || !url}
-                className="inset-y-2 right-2"
-              >
+      <form className=" w-full max-w-2xl flex items-center justify-center relative">
+        <Link size={16} color="#C9CED6" className="absolute left-6" />
+        <Input
+          type="url"
+          placeholder="Enter your link"
+          className="shorten-input"
+          onChange={(event) => setUrl(event.target.value)}
+          value={isCopyMode ? shortLink : url}
+        />
+        <PrimaryButton
+          type="submit"
+          disabled={isCopyMode ? isCopied : isLoading || !url}
+          callback={isCopyMode ? handleCopy : handleShortenUrl}
+        >
+          <span className="hidden lg:flex justify-center items-center">
+            {isCopyMode ? (
+              <>
                 <span>
-                  {isLoading && <SpinnerIcon height={24} width={24} />}
+                  {isCopied ? <FileCheck2 size={16} /> : <Copy size={16} />}
                 </span>
-                <span>{props.btnText}</span>
-              </PrimaryButton>
-            </div>
-            <PrimaryButton
-              type="submit"
-              callback={(event: FormEvent) => handleShortenUrl(event)}
-              disabled={isLoading || !url}
-              className="inset-y-2 right-2 lg:hidden w-[50px]"
-            >
-              <span>
-                {isLoading ? (
-                  <SpinnerIcon height={20} width={20} />
-                ) : (
-                  <ArrowRight size={20} />
-                )}
-              </span>
-            </PrimaryButton>
-          </form>
-        </>
-      )}
+                <span className="pl-2">{isCopied ? "Copied!" : "Copy"}</span>
+              </>
+            ) : (
+              <>
+                {isLoading && <SpinnerIcon height={24} width={24} />}
+                {btnText}
+              </>
+            )}
+          </span>
+          <span className="lg:hidden">
+            {isCopyMode ? (
+              isCopied ? (
+                <FileCheck2 size={16} />
+              ) : (
+                <Copy size={16} />
+              )
+            ) : isLoading ? (
+              <SpinnerIcon height={20} width={20} />
+            ) : (
+              <ArrowRight size={20} />
+            )}
+          </span>
+        </PrimaryButton>
+      </form>
     </section>
   );
 };
