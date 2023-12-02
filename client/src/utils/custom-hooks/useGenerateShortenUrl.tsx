96efa0c5163575ac9cc28577201283814d03ae3e
Naman Jawaliya <jawaliyan@gmail.com>
2023-12-02 17:16:29 +0530

    moved base url to env

--- client/src/utils/custom-hooks/useGenerateShortenUrl.tsx
+++ client/src/utils/custom-hooks/useGenerateShortenUrl.tsx
@@ -1,10 +1,10 @@
 import axios from "axios";
-import { API_BASE_URL } from "@/utils/constants/constants";
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 const useGenerateShortenUrl = () => {
   const generate = async (origUrl: string) => {
     console.log({ origUrl });
-    const endpoint = API_BASE_URL + "/short";
+    const endpoint = BASE_URL + "/short";
     try {
       const response = await axios.post(endpoint, {
         origUrl,
--- client/src/utils/custom-hooks/useGetAllUrls.tsx
+++ client/src/utils/custom-hooks/useGetAllUrls.tsx
@@ -1,13 +1,14 @@
-import { API_BASE_URL } from "@/utils/constants/constants";
 import axios from "axios";
 import { setAllUrls } from "@/utils/store/urlSlice";
 import { useAppDispatch } from "@/utils/store/appStore";
+
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 export const useGetAllUrls = () => {
   const dispatch = useAppDispatch();
 
   const getAllUrls = async () => {
-    const endpoint = API_BASE_URL + "/all";
+    const endpoint = BASE_URL + "/all";
     try {
       const response = await axios.get(endpoint);
 
