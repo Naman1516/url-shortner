3265b9f72bd458417f3148ee522e33c95b0f7721
Naman Jawaliya <jawaliyan@gmail.com>
2023-12-06 23:17:51 +0530

    hiding auth options on mobile

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -15,7 +15,7 @@ const Header = () => {
       <h1 className="font-extrabold bg-gradient-to-r from-[#eb568e] to-[#144ee3] text-transparent bg-clip-text uppercase">
         {BRAND_NAME}
       </h1>
-      <div className="flex gap-4">
+      <div className="hidden md:flex gap-4">
         <Button
           className="flex gap-2 px-8 py-4 rounded-full bg-[#181e29] border border-[#353c4a]"
           onClick={() => handleLoginRegisterClick("login")}
