69e60a73c4c87ca66fd0d6b2a5f258e6dda374a8
Naman Jawaliya <jawaliyan@gmail.com>
2023-12-11 21:49:07 +0530

    minor updates

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,8 +1,10 @@
+import LoginRegisterModal from "@/components/LoginRegisterModal";
+import HeaderMobileMenu from "@/components/HeaderMobileMenu";
+
 import { BRAND_NAME } from "@/utils/constants/constants";
-import LoginRegisterModal from "./LoginRegisterModal";
-import HeaderMobileMenu from "./HeaderMobileMenu";
 
 const Header = () => {
+
   return (
     <header className="h-16 flex justify-between items-center px-10 fixed inset-0 z-50">
       <h1 className="font-extrabold text-xl uppercase">{BRAND_NAME}</h1>
