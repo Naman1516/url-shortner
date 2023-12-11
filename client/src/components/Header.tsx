3948aadd12d5aac7bdc9990209f3ea794d2ac44f
Naman Jawaliya <jawaliyan@gmail.com>
2023-12-11 21:57:31 +0530

    hiding auth actions

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,20 +1,27 @@
+import { useState } from "react";
+
 import LoginRegisterModal from "@/components/LoginRegisterModal";
 import HeaderMobileMenu from "@/components/HeaderMobileMenu";
 
 import { BRAND_NAME } from "@/utils/constants/constants";
 
 const Header = () => {
+  const [showHeaderAction] = useState(false);
 
   return (
     <header className="h-16 flex justify-between items-center px-10 fixed inset-0 z-50">
       <h1 className="font-extrabold text-xl uppercase">{BRAND_NAME}</h1>
-      <div className="hidden lg:flex gap-2">
-        <LoginRegisterModal type="register" />
-        <LoginRegisterModal type="login" />
-      </div>
-      <div className="flex flex-col lg:hidden">
-        <HeaderMobileMenu />
-      </div>
+      {showHeaderAction && (
+        <>
+          <div className="hidden lg:flex gap-2">
+            <LoginRegisterModal type="register" />
+            <LoginRegisterModal type="login" />
+          </div>
+          <div className="flex flex-col lg:hidden">
+            <HeaderMobileMenu />
+          </div>
+        </>
+      )}
     </header>
   );
 };
